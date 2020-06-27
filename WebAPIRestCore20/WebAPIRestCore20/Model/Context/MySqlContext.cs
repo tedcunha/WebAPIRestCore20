6f1ed0d8915026d8b65cca1507edf90ab610394d
Ricardo Cunha <ricardocampitelhi@hotmail.com>
2020-06-27 18:50:38 -0300

    Conectar com o Banco de Dados criação do MySqlContext e o Context

---
+++ WebAPIRestCore20/WebAPIRestCore20/Model/Context/MySqlContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIRestCore20.Model.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext()
+        {
+        }
+
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+    }
+}
