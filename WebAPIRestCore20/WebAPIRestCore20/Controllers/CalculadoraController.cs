63c5f54e5720fe57e9ad54d4cf021c528e4b79ba
Ricardo Cunha <ricardocampitelhi@hotmail.com>
2020-06-27 10:16:59 -0300

    Implentação dos demais verbos da calculadora

--- WebAPIRestCore20/WebAPIRestCore20/Controllers/CalculadoraController.cs
+++ WebAPIRestCore20/WebAPIRestCore20/Controllers/CalculadoraController.cs
@@ -10,13 +10,83 @@ namespace WebAPIRestCore20.Controllers
     [Route("api/[controller]")]
     public class CalculadoraController : ControllerBase
     {
-        // GET api/values/5/5
-        [HttpGet("{primeiroNumero}/{segundoNumeroNumero}")]
-        public IActionResult Soma(string primeiroNumero, string segundoNumeroNumero)
+        // GET api/Somar/5/5
+        [HttpGet("Somar/{primeiroNumero}/{segundoNumeroNumero}")]
+        public IActionResult Somar(string primeiroNumero, string segundoNumeroNumero)
         {
             if (IsNumeric(primeiroNumero) && IsNumeric(segundoNumeroNumero))
             {
                 var soma = ConverteParaDecimal(primeiroNumero) + ConverteParaDecimal(segundoNumeroNumero);
+                return Ok(soma.ToString());
+            }
+
+            return BadRequest("Entrada de dados inválida!");
+        }
+
+        // GET api/Subtrair/5/5
+        [HttpGet("Subtrair/{primeiroNumero}/{segundoNumeroNumero}")]
+        public IActionResult Subtrair(string primeiroNumero, string segundoNumeroNumero)
+        {
+            if (IsNumeric(primeiroNumero) && IsNumeric(segundoNumeroNumero))
+            {
+                var soma = ConverteParaDecimal(primeiroNumero) - ConverteParaDecimal(segundoNumeroNumero);
+                return Ok(soma.ToString());
+            }
+
+            return BadRequest("Entrada de dados inválida!");
+        }
+
+        // GET api/Dividir/5/5
+        [HttpGet("Dividir/{primeiroNumero}/{segundoNumeroNumero}")]
+        public IActionResult Dividir(string primeiroNumero, string segundoNumeroNumero)
+        {
+            if (IsNumeric(primeiroNumero) && IsNumeric(segundoNumeroNumero))
+            {
+                if (ConverteParaDecimal(segundoNumeroNumero) == 0)
+                {
+                    return BadRequest("Numero não pode ser dividido por zero!");
+                }
+
+                var soma = ConverteParaDecimal(primeiroNumero) / ConverteParaDecimal(segundoNumeroNumero);
+                return Ok(soma.ToString());
+            }
+
+            return BadRequest("Entrada de dados inválida!");
+        }
+
+        // GET api/Multiplicar/5/5
+        [HttpGet("Multiplicar/{primeiroNumero}/{segundoNumeroNumero}")]
+        public IActionResult Multiplicar(string primeiroNumero, string segundoNumeroNumero)
+        {
+            if (IsNumeric(primeiroNumero) && IsNumeric(segundoNumeroNumero))
+            {
+                var soma = ConverteParaDecimal(primeiroNumero) * ConverteParaDecimal(segundoNumeroNumero);
+                return Ok(soma.ToString());
+            }
+
+            return BadRequest("Entrada de dados inválida!");
+        }
+
+        // GET api/Media/5/5
+        [HttpGet("Media/{primeiroNumero}/{segundoNumeroNumero}")]
+        public IActionResult Media(string primeiroNumero, string segundoNumeroNumero)
+        {
+            if (IsNumeric(primeiroNumero) && IsNumeric(segundoNumeroNumero))
+            {
+                var soma = ((ConverteParaDecimal(primeiroNumero) + ConverteParaDecimal(segundoNumeroNumero))/2);
+                return Ok(soma.ToString());
+            }
+
+            return BadRequest("Entrada de dados inválida!");
+        }
+
+        // GET api/RaizQuadrada/5/5
+        [HttpGet("RaizQuadrada/{primeiroNumero}/{segundoNumeroNumero}")]
+        public IActionResult RaizQuadrada(string primeiroNumero)
+        {
+            if (IsNumeric(primeiroNumero))
+            {
+                var soma = Math.Sqrt((double)ConverteParaDecimal(primeiroNumero));
                 return Ok(soma.ToString());
             }
 
