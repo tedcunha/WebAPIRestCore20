ac28e6ca82a8fcb5b3f330d98490812a0659f657
Ricardo Cunha <ricardocampitelhi@hotmail.com>
2020-07-02 18:25:00 -0300

    Alterações no HATEOAS

--- WebAPIRestCore20/WebAPIRestCore20/HyperMidia/PessoaEnricher.cs
+++ WebAPIRestCore20/WebAPIRestCore20/HyperMidia/PessoaEnricher.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Tapioca.HATEOAS;
 using WebAPIRestCore20.Data.VO;
@@ -10,48 +11,80 @@ namespace WebAPIRestCore20.HyperMidia
 {
     public class PessoaEnricher : ObjectContentResponseEnricher<PessoaVO>
     {
+
+        private readonly object _lock = new object();
+
         protected override Task EnrichModel(PessoaVO content, IUrlHelper urlHelper)
         {
             var path = "api/v1/pessoa";
             var url = new { controller = path, id = content.Id };
+            string link = getLink(urlHelper, path);
+            string linkWithId = getLink(content, urlHelper, path);
+
+            //content.Links.Add(new HyperMediaLink()
+            //{
+            //    Action = HttpActionVerb.GET,
+            //    Href = urlHelper.Link("DefaultApi", url),
+            //    Rel = RelationType.self,
+            //    Type = ResponseTypeFormat.DefaultGet
+            //});
 
             content.Links.Add(new HyperMediaLink()
             {
                 Action = HttpActionVerb.GET,
-                Href = urlHelper.Link("DefaultApi",url),
+                Href = link,
                 Rel = RelationType.self,
                 Type = ResponseTypeFormat.DefaultGet
             });
+
             content.Links.Add(new HyperMediaLink()
             {
                 Action = HttpActionVerb.GET,
-                Href = urlHelper.Link("DefaultApi", url),
+                Href = linkWithId,
                 Rel = RelationType.self,
                 Type = ResponseTypeFormat.DefaultGet
             });
             content.Links.Add(new HyperMediaLink()
             {
                 Action = HttpActionVerb.POST,
-                Href = urlHelper.Link("DefaultApi", url),
+                Href = link,
                 Rel = RelationType.self,
                 Type = ResponseTypeFormat.DefaultPost
             });
             content.Links.Add(new HyperMediaLink()
             {
                 Action = HttpActionVerb.PUT,
-                Href = urlHelper.Link("DefaultApi", url),
+                Href = link,
                 Rel = RelationType.self,
                 Type = ResponseTypeFormat.DefaultPost
             });
             content.Links.Add(new HyperMediaLink()
             {
                 Action = HttpActionVerb.DELETE,
-                Href = urlHelper.Link("DefaultApi", url),
+                Href = linkWithId,
                 Rel = RelationType.self,
                 Type = "int",
             });
 
             return null;
+        }
+
+        private string getLink(IUrlHelper urlHelper, string path)
+        {
+            lock (_lock)
+            {
+                var url = new { controller = path };
+                return new StringBuilder(urlHelper.Link("DefaultApi", url)).ToString();
+            }
+        }
+
+        private string getLink(PessoaVO content, IUrlHelper urlHelper, string path)
+        {
+            lock (_lock)
+            {
+                var url = new { controller = path, id = content.Id };
+                return new StringBuilder(urlHelper.Link("DefaultApi", url)).ToString();
+            }
         }
     }
 }
